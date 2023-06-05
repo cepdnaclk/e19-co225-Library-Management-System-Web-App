f83d2c1729086d96ae0a85286b89d286c1e0c4e3
akashwi <e19453@eng.pdn.ac.lk>
2023-06-05 20:06:21 +0530

    Initialized Spring boot project

---
+++ src/test/java/com/e19/librarymanagement/LibrarymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e19.librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
