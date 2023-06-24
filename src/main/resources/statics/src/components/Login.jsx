a16975b307efd7ce36718c807bc0edb10637dc46
akashwi <e19453@eng.pdn.ac.lk>
2023-06-25 04:00:58 +0530

    Registration and login page connected to backend

--- src/main/resources/statics/src/components/Login.jsx
+++ src/main/resources/statics/src/components/Login.jsx
@@ -1,12 +1,36 @@
 import React, { useState } from "react";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './form.css';
 
 export const Login = (props) => {
+    const navigate = useNavigate();
+
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(email);
+
+        const formData = {
+            email: email,
+            password: pass
+          };
+        
+          axios.post('http://localhost:8080/api/v1/auth/authenticate', formData)
+            .then(response => {
+              console.log(response.data);
+
+                const accessToken = response.data.access_token;
+                if (accessToken) {
+                    localStorage.setItem('accessToken', accessToken);
+                    navigate('/booklist');
+                }
+            })
+            .catch(error => {
+              console.error(error);
+            });
     }
 
     return (
@@ -17,7 +41,7 @@ export const Login = (props) => {
                 <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Log In</button>
+                <button className="register-btn" type="submit">Log In</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
         </div>
