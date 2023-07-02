67ab003ad9bad6284b26efc2f686a8b7dc9ca64b
Bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2023-07-02 17:34:45 +0530

    Update EmailJob.java

--- src/main/resources/statics/src/components/EmailJob.java
+++ src/main/resources/statics/src/components/EmailJob.java
@@ -1,9 +1,14 @@
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class EmailJob implements Job {
 
@@ -11,6 +16,57 @@ public class EmailJob implements Job {
     private List<User> getUserList() {
         // Implement the logic to fetch the list of users
         // from the database or data source and return it
+        List<User> userList = new ArrayList<>();
+        
+        // Example: Fetch users from a database
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+        // Create a database connection
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "username", "password");
+
+        // Execute a query to fetch users
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery("SELECT * FROM users");
+
+        // Iterate over the result set and create User objects
+        while (resultSet.next()) {
+            String username = resultSet.getString("username");
+            String email = resultSet.getString("email");
+
+            User user = new User(username, email);
+            userList.add(user);
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    } finally {
+        // Close the database resources
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    return userList;
     }
 
     @Override
@@ -59,16 +115,41 @@ public class EmailJob implements Job {
             }
         });
 
-        // Create a new email message
-        Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("your_email"));
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
-        message.setSubject("Library Book Due Date Reminder");
-        message.setText("Dear User,\n\nThis is a reminder that the book \"" + bookTitle +
-                "\" is due tomorrow (" + dueDate.toString() + ").\n\nPlease return the book to the library.\n\nBest regards,\nThe Library Team");
+        try {
+            // Create a new email message
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("your_email"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
+            message.setSubject("Library Book Due Date Reminder");
+            message.setText("Dear User,\n\nThis is a reminder that the book \"" + bookTitle +
+                    "\" is due tomorrow (" + dueDate.toString() + ").\n\nPlease return the book to the library.\n\nBest regards,\nThe Library Team");
 
-        // Send the email
-        Transport.send(message);
+            // Send the email
+            Transport.send(message);
+        } catch (MessagingException e) {
+            // Handle any exceptions or errors that occur during the process
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+
+            JobDetail job = JobBuilder.newJob(EmailJob.class)
+                    .withIdentity("emailJob", "group")
+                    .build();
+
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .withIdentity("emailTrigger", "group")
+                    .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(0, 0))  // Run every day at midnight
+                    .build();
+
+            scheduler.scheduleJob(job, trigger);
+            scheduler.start();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
     }
 }
 
