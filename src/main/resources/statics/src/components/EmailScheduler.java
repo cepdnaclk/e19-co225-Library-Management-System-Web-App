015258319a040eb42f3df4572d28b0505b47529a
Bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2023-07-02 14:44:59 +0530

    Update EmailScheduler.java

--- src/main/resources/statics/src/components/EmailScheduler.java
+++ src/main/resources/statics/src/components/EmailScheduler.java
@@ -1,3 +1,83 @@
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class EmailJob implements Job {
+
+    // Method to retrieve the list of users from the database or data source
+    private List<User> getUserList() {
+        // Implement the logic to fetch the list of users
+        // from the database or data source and return it
+    }
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) {
+        // Logic to calculate due dates and send reminder emails
+        try {
+            // Get the list of users and their books from your database or data source
+            List<User> users = getUserList();
+
+            // Iterate over each user
+            for (User user : users) {
+                List<Book> borrowedBooks = user.getBorrowedBooks();
+
+                // Iterate over each book borrowed by the user
+                for (Book book : borrowedBooks) {
+                    Date dueDate = book.getDueDate();
+                    Date currentDate = new Date();
+
+                    // Calculate the time difference between the current date and due date
+                    long timeDifference = dueDate.getTime() - currentDate.getTime();
+                    long daysDifference = TimeUnit.DAYS.convert(timeDifference, TimeUnit.MILLISECONDS);
+
+                    // If the due date is tomorrow, send a reminder email
+                    if (daysDifference == 1) {
+                        sendReminderEmail(user.getEmail(), book.getTitle(), dueDate);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // Handle any exceptions or errors that occur during the process
+            e.printStackTrace();
+        }
+    }
+
+    private void sendReminderEmail(String recipientEmail, String bookTitle, Date dueDate) {
+        // Configure the email server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "your_smtp_host");
+        properties.put("mail.smtp.port", "your_smtp_port");
+
+        // Create a Session with authentication credentials
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("your_email", "your_password");
+            }
+        });
+
+        try {
+            // Create a new email message
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("your_email"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
+            message.setSubject("Library Book Due Date Reminder");
+            message.setText("Dear User,\n\nThis is a reminder that the book \"" + bookTitle +
+                    "\" is due tomorrow (" + dueDate.toString() + ").\n\nPlease return the book to the library.\n\nBest regards,\nThe Library Team");
+
+            // Send the email
+            Transport.send(message);
+        } catch (MessagingException e) {
+            // Handle any exceptions or errors that occur during the process
+            e.printStackTrace();
+        }
+    }
+}
+
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
