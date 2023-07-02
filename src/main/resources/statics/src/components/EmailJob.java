44c285c68d092e3cb8c54f702f595004065fb105
Bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2023-07-02 14:27:44 +0530

    Update EmailJob.java

--- src/main/resources/statics/src/components/EmailJob.java
+++ src/main/resources/statics/src/components/EmailJob.java
@@ -1,66 +1,74 @@
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 public class EmailJob implements Job {
-@Override
-public void execute(JobExecutionContext jobExecutionContext) {
-    // Logic to calculate due dates and send reminder emails
-    try {
-        // Get the list of users and their books from your database or data source
-        List<User> users = getUserList();
 
-        // Iterate over each user
-        for (User user : users) {
-            List<Book> borrowedBooks = user.getBorrowedBooks();
+    // Method to retrieve the list of users from the database or data source
+    private List<User> getUserList() {
+        // Implement the logic to fetch the list of users
+        // from the database or data source and return it
+    }
 
-            // Iterate over each book borrowed by the user
-            for (Book book : borrowedBooks) {
-                Date dueDate = book.getDueDate();
-                Date currentDate = new Date();
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) {
+        // Logic to calculate due dates and send reminder emails
+        try {
+            // Get the list of users and their books from your database or data source
+            List<User> users = getUserList();
 
-                // Calculate the time difference between the current date and due date
-                long timeDifference = dueDate.getTime() - currentDate.getTime();
-                long daysDifference = TimeUnit.DAYS.convert(timeDifference, TimeUnit.MILLISECONDS);
+            // Iterate over each user
+            for (User user : users) {
+                List<Book> borrowedBooks = user.getBorrowedBooks();
 
-                // If the due date is tomorrow, send a reminder email
-                if (daysDifference == 1) {
-                    sendReminderEmail(user.getEmail(), book.getTitle(), dueDate);
+                // Iterate over each book borrowed by the user
+                for (Book book : borrowedBooks) {
+                    Date dueDate = book.getDueDate();
+                    Date currentDate = new Date();
+
+                    // Calculate the time difference between the current date and due date
+                    long timeDifference = dueDate.getTime() - currentDate.getTime();
+                    long daysDifference = TimeUnit.DAYS.convert(timeDifference, TimeUnit.MILLISECONDS);
+
+                    // If the due date is tomorrow, send a reminder email
+                    if (daysDifference == 1) {
+                        sendReminderEmail(user.getEmail(), book.getTitle(), dueDate);
+                    }
                 }
             }
+        } catch (Exception e) {
+            // Handle any exceptions or errors that occur during the process
+            e.printStackTrace();
         }
-    } catch (Exception e) {
-        // Handle any exceptions or errors that occur during the process
-        e.printStackTrace();
+    }
+
+    private void sendReminderEmail(String recipientEmail, String bookTitle, Date dueDate) throws MessagingException {
+        // Configure the email server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "your_smtp_host");
+        properties.put("mail.smtp.port", "your_smtp_port");
+
+        // Create a Session with authentication credentials
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("your_email", "your_password");
+            }
+        });
+
+        // Create a new email message
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("your_email"));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
+        message.setSubject("Library Book Due Date Reminder");
+        message.setText("Dear User,\n\nThis is a reminder that the book \"" + bookTitle +
+                "\" is due tomorrow (" + dueDate.toString() + ").\n\nPlease return the book to the library.\n\nBest regards,\nThe Library Team");
+
+        // Send the email
+        Transport.send(message);
     }
 }
 
-private void sendReminderEmail(String recipientEmail, String bookTitle, Date dueDate) throws MessagingException {
-    // Use JavaMail API to send the email
-    // Configure the email server properties
-    Properties properties = new Properties();
-    properties.put("mail.smtp.host", "your_smtp_host");
-    properties.put("mail.smtp.port", "your_smtp_port");
-
-    // Create a Session with authentication credentials
-    Session session = Session.getInstance(properties, new Authenticator() {
-        @Override
-        protected PasswordAuthentication getPasswordAuthentication() {
-            return new PasswordAuthentication("your_email", "your_password");
-        }
-    });
-
-    // Create a new email message
-    Message message = new MimeMessage(session);
-    message.setFrom(new InternetAddress("your_email"));
-    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
-    message.setSubject("Library Book Due Date Reminder");
-    message.setText("Dear User,\n\nThis is a reminder that the book \"" + bookTitle +
-            "\" is due tomorrow (" + dueDate.toString() + ").\n\nPlease return the book to the library.\n\nBest regards,\nThe Library Team");
-
-    // Send the email
-    Transport.send(message);
-}
-
-}
